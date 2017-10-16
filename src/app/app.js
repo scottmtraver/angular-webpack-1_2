f274ed3a51692b29d1139b2dc46cf47e7d891ef0
Scott Traver <straver@artemishealth.com>
2017-10-16 13:38:22 -0600

    Add css into webpack

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,14 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './app.css';
 
 function component () {
 
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['hallo', 'Welt'], ' ');
+    element.classList.add('bonjour');
 
     return element;
 }
 
-document.body.appendChild(component())
+document.body.appendChild(component());
