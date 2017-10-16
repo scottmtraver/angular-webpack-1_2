ab3bd0800ec2c3f8be5b126c6d9eb03bff30439d
Scott Traver <straver@artemishealth.com>
2017-10-16 14:06:43 -0600

    Webpack images, fonts, and data

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,27 @@
 import _ from 'lodash';
 import './app.css';
+import Icon from '../assets/images/pug.png';
+import Data from '../assets/data/data.json';
 
 function component () {
 
     var element = document.createElement('div');
 
-    element.innerHTML = _.join(['hallo', 'Welt'], ' ');
+    // from data file
+    _.forEach(Data, (g) => {
+        let span = document.createElement('span');
+        span.innerHTML = g.greeting;
+        element.appendChild(span);
+    })
+
+    // styles (basic css not compiled)
     element.classList.add('bonjour');
+
+    // images
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
