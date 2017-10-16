fa4be4df547a4211484016343f0bcf83a4a06743
Scott Traver <straver@artemishealth.com>
2017-10-16 14:26:58 -0600

    Working with output plugins

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import printMe from './utilities/print';
 
 function component () {
 
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['hallo', 'Welt'], ' ');
+
+
+    btn.innerHTML = 'Press Here';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/app/app.js',
+    entry: {
+        app: './src/app/app.js',
+        print: './src/app/utilities/print.js',
+
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
 };
