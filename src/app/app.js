453c9235ba045158e9c8e53f64b7ad22f8d03130
Scott Traver <straver@artemishealth.com>
2017-10-16 14:14:43 -0600

    Remove assets ahead of webpack output guide

--- src/app/app.js
+++ src/app/app.js
@@ -1,27 +1,10 @@
 import _ from 'lodash';
-import './app.css';
-import Icon from '../assets/images/pug.png';
-import Data from '../assets/data/data.json';
 
 function component () {
 
     var element = document.createElement('div');
 
-    // from data file
-    _.forEach(Data, (g) => {
-        let span = document.createElement('span');
-        span.innerHTML = g.greeting;
-        element.appendChild(span);
-    })
-
-    // styles (basic css not compiled)
-    element.classList.add('bonjour');
-
-    // images
-    var myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
+    element.innerHTML = _.join(['hallo', 'Welt'], ' ');
 
     return element;
 }
